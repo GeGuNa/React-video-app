161118e04adf98a55cb5afa66474ab321231f262
Phantom <abracadabra1253@gmail.com>
2023-11-22 00:52:18 +0400

    password recovery

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Switch, Route, Link } from 'wouter';
 import Home from './pages/index.js';
 import Video from './pages/video.js';
 import Login from './pages/login.js';
+import Recovery from './pages/recovery.js';
+
+
 
 export default function App() {
   return (<>
@@ -10,6 +13,7 @@ export default function App() {
 		<Route path="/" component={Home} />
 		<Route path="/video/:id" component={Video} />
 		<Route path="/login" component={Login} />
+		<Route path="/recovery" component={Recovery} />
 		<Route>404, Page not been found</Route>
    </Switch>
    
---
+++ src/pages/recovery.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import '../components/css/main.css';
+import  { Header } from '../components/js/Header.js';
+import  { Main } from '../components/js/Main.js';
+import  { Left } from '../components/js/Left.js';
+import  { Right } from '../components/js/Right.js';
+import  { Footer } from '../components/js/Footer.js';
+
+
+
+export default function Recovery() {
+	
+	return (<>
+	
+<Header/>	
+	
+	
+<Main>
+
+<Left idName="left" />
+
+
+
+
+<Right> 
+
+
+<div>
+		
+		
+		
+<div className="mm__qqqtpq10"></div>
+
+
+
+
+
+			<div class="logg_qiiin">
+
+
+<div class="logg_qiiin2z">
+
+
+	<div class="qqllzcnq1_qq">	Password recovery	</div>
+
+
+
+
+
+<div class="pddfql1">
+
+<form>
+
+<div class="frm12">
+<label class="foqmzmbold">Username</label>
+<input class="kkkqqzl_qitp1" type="text" placeholder="Username"/>
+</div>
+
+<div class="frm12">
+<label class="foqmzmbold">Email </label>
+<input  class="kkkqqzl_qitp1" type="text" placeholder="Enter your email address"/>
+</div>
+
+
+<br/>
+<div class="kfqm_bnt1">
+	<button type="submit">Request</button>
+</div>
+
+</form>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+</div>
+
+							
+
+
+
+
+				</div>	
+	
+
+	
+
+
+
+	
+
+
+	</div>
+
+
+
+
+
+</Right>
+
+
+</Main>	
+
+
+<Footer/>
+	
+	
+	</>)
+}
